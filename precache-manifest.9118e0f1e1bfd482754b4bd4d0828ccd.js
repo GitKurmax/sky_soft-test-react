03b50cc0843653491649f26b7cf60bdee81e4ee6
GitKurmalex <alexandrmail32@gmail.com>
2019-07-10 12:15:43 +0300

    Updates

---
+++ precache-manifest.9118e0f1e1bfd482754b4bd4d0828ccd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17e57a3e1984d560ee312b12e6859ca2",
+    "url": "/sky_soft-test-react/index.html"
+  },
+  {
+    "revision": "fc1a04f5442fc70ce71f",
+    "url": "/sky_soft-test-react/static/js/2.f9075ab4.chunk.js"
+  },
+  {
+    "revision": "d7669665b47fc7af23eb",
+    "url": "/sky_soft-test-react/static/js/main.b7d9c11a.chunk.js"
+  },
+  {
+    "revision": "19ea50d1a04ae744b83b",
+    "url": "/sky_soft-test-react/static/js/runtime~main.ed7f457b.js"
+  }
+]);
